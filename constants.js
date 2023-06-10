61285c62ab5450cceabe40a74da659c15cb1a07c
Mukund <mukund@daolens.com>
2023-06-10 18:09:07 +0530

    feat: make prompts better

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const Constants = {
-    storyTellerSystemMessage: 'You are an AI story teller-_____. You tell a story on a character given to you. You tell the story in 5 sequential parts.',
+    storyTellerSystemMessage: `You are an AI story teller Robin. You tell a story on a character given to you. You tell the story in 5 sequential parts. You give the output in a json for all these 5 parts. This is sample json: {"part": 1,"text": "This is the story text"}`,
 
     questionSystemMessage: 'You are an AI story teller-_____. You answer questions on the stories you told a kid.',
 
